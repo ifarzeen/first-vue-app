6fa621af4db53c3a662df5203fda78d6b128031d
ifarzeen <farzeensaleem288@gmail.com>
2020-10-28 16:13:00 +0500

    test-2

---
+++ test-project/src/App.vue
@@ -0,0 +1,65 @@
+<template>
+  <div id="app">
+
+    <Header/>
+
+    <AddTodo/>
+
+    <Todos v-bind:todos="todos" 
+    v-on:del-todo="deleteToDo"/>
+    
+  </div>
+</template>
+
+<script>
+
+import Header from './components/Layout/header';
+import Todos from './components/Todos'
+import AddTodo from './components/AddTodo'
+
+
+export default {
+  name: 'App',
+  components: {
+     Header,
+     Todos,
+     AddTodo
+  },
+  data(){
+    return {
+      todos:[
+        {
+          id:1,
+          title: "Todo One",
+          completed:false
+      },
+      {
+          id:2,
+          title: "Todo Two",
+          completed:true
+      },
+      {
+          id:3,
+          title: "Todo Three",
+          completed:true
+      }
+      ]
+    } 
+     
+    },
+
+    methods:{
+      deleteToDo(id){
+        this.todos=this.todos.filter(todo => todo.id !== id);
+      }
+    }
+  }
+</script>
+
+<style>
+*{
+  box-sizing: border-box;
+  padding: 0;
+  margin: 0;
+}
+</style>
